7be5d9ded079b66f259d45b16dc5cd13f1bc20b2
Taosha <taosha90@gmail.com>
2015-11-22 14:11:34 -0500

    Update Length of Last Word_builtinpythonfunc.py

--- Length of Last Word_builtinpythonfunc.py
+++ Length of Last Word_builtinpythonfunc.py
@@ -5,3 +5,8 @@ class Solution(object):
         :rtype: int
         """
         return len(s.strip().split(" ")[-1])
+        
+#### strip() removes leading and ending whitespaces
+#### split(' ') split the string with a single ' ', e.g: "   a   ".split(' ') gives ['', '', '', 'a', '', '', '']
+#### split() : consective whitespaces are considered as a single seperator, and ' '.split() gives []
+#### so here I need to use function  '.split(' ')' to retrive [-1] of the resulting list, which cannot be empty.
